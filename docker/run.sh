ca33fde64ea65931cfc34d7fd86f795c507d6a5a
Stephen van Rooyen <vanrooyensm@gmail.com>
2026-02-25 11:28:45 +0200

    Closes #1

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,8 @@ while [[ $# -gt 0 ]]; do
   case "$1" in
     --env-file|-e)
       ADDITIONAL_ENV_FILE="$2"; shift 2 ;;
+    --jdbc-driver|-j)
+      JDBCDRIVER="$2"; shift ;;
     *)
       shift ;;
   esac
@@ -11,5 +13,44 @@ done
 
 source "$(dirname "$0")/_init.sh" "$ADDITIONAL_ENV_FILE"
 
-echo "Running Docker container ${CONTAINER_NAME} from image ${CONTAINER_IMAGE}:${CONTAINER_TAG}..."
-echo "docker run  --detach  --name "${CONTAINER_NAME}" --publish "${HOST_PORT}:${CONTAINER_PORT}"  --restart unless-stopped --env-file "$ENV_FILE" "${CONTAINER_IMAGE}:${CONTAINER_TAG}""
+CONTAINER_TAG=${CONTAINER_TAG:-${APP_VERSION}}
+CONTAINER_PORT=${CONTAINER_PORT:-$OPTION_SERVERPORT}
+HOST_PORT=${HOST_PORT:-${CONTAINER_PORT:-$OPTION_SERVERPORT}}
+VOL_LIST=""
+VOL_PARAMS=""
+if [[ -n "$JDBCDRIVER" ]]; then
+  HOST_PATH=$JDBCDRIVER
+  CONTAINER_PATH=${OPTION_DB_JDBCDRIVERFILES:-/tmp/driver.jar}
+  VOL_PARAMS+="    --volume \"$HOST_PATH:$CONTAINER_PATH\" \\"$'\n'
+  HOST_PATH=$(echo "$JDBCDRIVER" | sed -e 's/\\\\/\x01/g' -e 's/\\/\\\\\\\\/g' -e 's/\x01/\\\\/g')
+  VOL_LIST=${VOL_LIST:-"    volumes:"}$'\\n'
+  VOL_LIST+="      - \"$HOST_PATH:$CONTAINER_PATH\""$'\\n'
+fi
+
+ENV_LIST=""
+ENV_PARAMS=""
+while read -r key; do
+  [[ -z "$key" ]] && continue
+  eval "value=\$$key"
+  value_escaped=$(printf '%s' "$value" | sed -e 's/[\/&]/\\&/g' -e 's/\\/\\\\/g')
+  ENV_LIST+="      - $key=$value"$'\\n'
+  value_escaped=$(printf '%q' "$value" | sed 's|^/|\\\\\\/|g')
+  ENV_PARAMS+="    --env $key=$value_escaped \\"$'\n'
+done < <(grep -hv '^#' "${SOURC_ENV_FILE[@]}" | cut -d= -f1 | sort -u)
+
+sed \
+  -e "s|__CONTAINER_GROUP__|${CONTAINER_GROUP}|g" \
+  -e "s|__CONTAINER_NAME__|${CONTAINER_NAME}|g" \
+  -e "s|__CONTAINER_IMAGE__|${CONTAINER_IMAGE}|g" \
+  -e "s|__CONTAINER_TAG__|${CONTAINER_TAG}|g" \
+  -e "s|__CONTAINER_RESTART__|${CONTAINER_RESTART}|g" \
+  -e "s|__CONTAINER_PORT__|${CONTAINER_PORT}|g" \
+  -e "s|__HOST_PORT__|${HOST_PORT}|g" \
+  -e "s|__ENV_LIST__|$ENV_LIST|g" \
+  -e "s|__VOL_LIST__|$VOL_LIST|g" \
+  "$TEMPLATE_FILE" > "$YAML_FILE"
+
+echo "Use the following command to run the container ${CONTAINER_NAME} from image ${CONTAINER_IMAGE}:${CONTAINER_TAG}..."
+echo "docker-compose --file \"${YAML_FILE}\" up --detach"
+docker-compose --file "${YAML_FILE}" up --detach
+#echo -e "docker run --detach \\\\\n    --name \"${CONTAINER_NAME}\" \\\\\n    --publish ${HOST_PORT}:${CONTAINER_PORT} \\\\\n    --restart ${CONTAINER_RESTART} \\\\\n${ENV_PARAMS}${VOL_PARAMS}    \"${CONTAINER_IMAGE}:${CONTAINER_TAG}\""
